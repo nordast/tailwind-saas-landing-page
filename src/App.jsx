64d88bfd99b995ed67c91ce2c1900ca22ba2ebb6
nordast <nordast@gmail.com>
2024-12-09 13:27:20 +0200

    hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import Header from './sections/Header.jsx'
+import Header from "./sections/Header.jsx";
+import Hero from "./sections/Hero.jsx";
 
 const App = () => {
   return (
     <div>
       <main className="overflow-hidden">
         <Header />
+        <Hero />
       </main>
     </div>
   );
